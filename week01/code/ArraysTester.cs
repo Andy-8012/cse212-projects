7a5e61f562f9c0fa2eefc731db94be36e4d7ffde
Andy-8012 <andrewvriley@gmail.com>
2023-10-30 19:19:08 -0600

    updated it

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -39,10 +39,10 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        //step 1 Create an empty array to hold the multples of your number using the length variable from the function
+        //step 1 Create an empty array to hold the multiples of your number using the length variable from the function
         double[] multiples = new double[length];
 
-        //step 2 Create a for loop and use the length for the number of iterations plus 1
+        //step 2 Create a for loop and use the length for the number of iterations
         for (var i = 0; i < length; ++i) {
 
             //step 3 In the for loop use the number from the function and multiply it by
